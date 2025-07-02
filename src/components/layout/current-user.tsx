6196eed72c113b4f72f87c4de9ab7e006a2b1d9a
Eric Blanchette <W0517870@myscc.ca>
2025-07-02 16:09:04 -0400

    added button to user

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,17 +1,37 @@
-import { Popover } from 'antd'
+import { Button, Popover } from 'antd'
 import React from 'react'
 import CustomAvatar from '../custom-avatar'
 import { useGetIdentity } from '@refinedev/core'
 import type {User} from '@/graphql/schema.types'
+import { Text } from './text'
+import { SettingOutlined } from '@ant-design/icons'
 
 const CurrentUser = () => {
     const { data: user } = useGetIdentity<User>();
-
+    const [isOpen, setIsOpen] = React.useState(false);
     const content = (
       <div style={{
         display: 'flex',
         flexDirection: 'column',
       }}>
+        <Text
+        strong
+        style={{ padding: '12px 20px'}}
+        >
+          {user?.name}
+        </Text>
+
+        <div>
+          <Button
+            style={{ textAlign: 'left'}}
+            icon={<SettingOutlined />}
+            type="text"
+            block 
+            onClick={() => setIsOpen(true)}
+          >
+            Account Settings
+          </Button>
+        </div>
 
       </div>
     )
